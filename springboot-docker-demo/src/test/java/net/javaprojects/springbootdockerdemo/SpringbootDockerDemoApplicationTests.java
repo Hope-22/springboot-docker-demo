386dde80641d4de034c7457994b060780f6ab4a8
Hope Onofe <onofehope107@gmail.com>
2024-02-15 12:27:15 +0100

    first commit

---
+++ springboot-docker-demo/src/test/java/net/javaprojects/springbootdockerdemo/SpringbootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaprojects.springbootdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
